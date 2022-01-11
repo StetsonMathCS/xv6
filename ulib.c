b67e930740d49fbcd989bfbd9d18d3ba6fa37ddc
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2022-01-11 12:46:08 -0500

    Added itoa to ulibc.c

--- ulib.c
+++ ulib.c
@@ -104,3 +104,65 @@ memmove(void *vdst, const void *vsrc, int n)
     *dst++ = *src++;
   return vdst;
 }
+
+void swap(char *xp, char *yp)
+{
+	char temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+// https://www.geeksforgeeks.org/implement-itoa/
+void reverse(char str[], int length)
+{
+	int start = 0;
+	int end = length -1;
+	while (start < end)
+	{
+		swap(str+start, str+end);
+		start++;
+		end--;
+	}
+}
+
+// Implementation of itoa()
+void itoa(int num, char* str)
+{
+    int base = 10;
+	int i = 0;
+	int isNegative = 0;
+
+	/* Handle 0 explicitly, otherwise empty string is printed for 0 */
+	if (num == 0)
+	{
+		str[i++] = '0';
+		str[i] = '\0';
+		return;
+	}
+
+	// In standard itoa(), negative numbers are handled only with
+	// base 10. Otherwise numbers are considered unsigned.
+	if (num < 0 && base == 10)
+	{
+		isNegative = 1;
+		num = -num;
+	}
+
+	// Process individual digits
+	while (num != 0)
+	{
+		int rem = num % base;
+		str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+		num = num/base;
+	}
+
+	// If number is negative, append '-'
+	if (isNegative)
+		str[i++] = '-';
+
+	str[i] = '\0'; // Append string terminator
+
+	// Reverse the string
+	reverse(str, i);
+}
+
